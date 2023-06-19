45a0fcabd08830a9870a6e95e26b8711e55f6899
Subhajit Chatterjee <subhajt.bdk@gmail.com>
2023-06-19 23:48:03 +0530

    Counter

---
+++ project 2 (counter)/app.js
@@ -0,0 +1,36 @@
+//set initial count
+let count = 0;
+
+
+// select value and buttons 
+const value =document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+
+btns.forEach(function(btn){
+    btn.addEventListener('click',function(e){
+        // console.log(e.currentTarget.classList);
+        const styles = e.currentTarget.classList;
+        if(styles.contains('decrease')){
+            count--;
+        }
+        else if(styles.contains('reset')){
+            count=0;
+        }
+        else{
+            count++;
+        }
+
+
+        if (count>0){
+            value.style.color = "green";
+        }
+        else if (count<0){
+            value.style.color = "red";
+        }
+        else{
+            value.style.color = "#222";
+        }
+        value.textContent = count;
+    });
+});
